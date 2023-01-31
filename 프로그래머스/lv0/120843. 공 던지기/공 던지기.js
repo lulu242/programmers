10f85a5e71f5b6d48160a8c27486ff536713b836
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-01-31 16:24:19 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,13 +1,3 @@
-function solution(numbers, k) {
-    var answer = 1;
-    for(let i = 1; i < k ; i++){
-        if(answer === numbers.length-1){
-            answer = 1;
-        } else if(answer === numbers.length){
-            answer = 2;
-        } else {
-            answer += 2
-        }
-    } 
-    return numbers[answer-1];
+function solution(numbers, k) {  
+    return numbers[((k-1)*2) % numbers.length]
 }
