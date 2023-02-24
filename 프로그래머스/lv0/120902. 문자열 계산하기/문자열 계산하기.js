be2a5226b142027f528a7e29c01266a8588fcabf
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-02-25 00:14:35 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,14 +1,5 @@
 function solution(my_string) {
- 
-    let arr = my_string.split(' ')
-    var answer = +arr[0]
-    
-    for(let i = 1; i < arr.length-1 ; i++){
-        if(arr[i] === '+'){
-            answer += +arr[i+1]
-        } else if(arr[i] === '-'){
-            answer -= +arr[i+1]
-        }
-    }
-    return answer;
+    const arr = my_string.split(' ')
+    while(arr.length > 1) arr.unshift(+arr.shift() + (arr.shift() === "+" ? 1 : -1) * arr.shift())
+    return arr[0]
 }
