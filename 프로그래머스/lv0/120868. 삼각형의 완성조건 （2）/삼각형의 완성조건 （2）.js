8fe1081f22aa53817409333d048a64f2b49b534a
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-03-06 22:33:03 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -0,0 +1,9 @@
+function solution(sides) {
+    // 마지막 변이 가장 긴 변일 경우: 두변 합 > 긴 변 > 둘중 긴변     => 합 - 긴변 -1
+    // 둘중 하나가 긴변일 경우: 긴변 - 짧은 변 < 마지막변 <= 긴 변    => 긴변 - 기변 + 짧은 변
+    var answer = 0;
+    const max = Math.max(...sides);
+    const min = Math.min(...sides);
+    answer = 2 * min - 1
+    return answer;
+}
