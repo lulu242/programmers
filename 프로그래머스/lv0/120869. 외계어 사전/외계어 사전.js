808bc1e39d18f99f01dcf5d4f0bd61e4b993823c
lulu242 <119933024+lulu242@users.noreply.github.com>
2023-03-08 22:50:28 +0900

    [level 0] Title: 외계어 사전, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,4 @@
 function solution(spell, dic) {
-    var answer = 0;
-    dic = dic.filter(el => el.length === spell.length);
-    for(let i of spell) {
-        dic = dic.map(el => el.replace(i, ''));
-    }
-    answer = dic.includes('') ? 1 : 2;
-    return answer;
-    
+    // dic 중에서 정렬했을 때 spell 정렬한 값이랑 같은게 있다면 1 없다면 2 
+    return dic.some(el => [...el].sort().toString() === spell.sort().toString()) ? 1 : 2
 }
