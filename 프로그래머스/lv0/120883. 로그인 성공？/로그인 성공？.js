61e19682ff0248fe620b49b36b2a4146921765e8
lulu242 <kgsol3242@gmail.com>
2023-03-09 22:04:43 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,8 +1,8 @@
 function solution(id_pw, db) {
-
-    const flat = db.flat()
-    const idx = flat.findIndex(el => el === id_pw[0]);
-    if(idx === -1) return 'fail'
-    return flat[idx + 1] === id_pw[1] ? 'login' : 'wrong pw'
-
+  const [id, pw] = id_pw
+  //Map 객체는 키-값 쌍인 집합이면 키는 단 하나만 존재한다
+  // set으로 추가, get으로 값 반환, has로 존재여부확인
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+    
 }
