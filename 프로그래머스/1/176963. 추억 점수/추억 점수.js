201ec639d564d4a7400e52d40e12eb16cf036232
lulu242 <kgsol3242@gmail.com>
2023-10-19 23:43:46 +0900

    [level 1] Title: 추억 점수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,11 +1,8 @@
 function solution(name, yearning, photo) {
     for(let i = 0; i < photo.length; i++) {
         for(let j = 0; j < photo[i].length; j++) {
-            if(name.indexOf(photo[i][j]) >= 0 ) {
-                photo[i][j] = yearning[name.indexOf(photo[i][j])]
-            } else {
-                photo[i][j] = 0
-            }
+            photo[i][j] = yearning[name.indexOf(photo[i][j])] ?? 0
+            } 
         }
-    } return photo.map(el => el.reduce((acc, cur)=> acc + cur))
+    return photo.map(el => el.reduce((acc, cur)=> acc + cur))
 }
