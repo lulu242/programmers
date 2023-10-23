c0bc549bf9b01aeba0458f5d9ea04770f63dc220
lulu242 <kgsol3242@gmail.com>
2023-10-23 22:51:15 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,26 @@
+function solution(s) {
+    let answer = 0;
+    let bracket = {"(":")", "[":"]", "{":"}"};
+    
+    function isRight(str) {
+        let copy = [];
+        for(let el of str) {
+            if(bracket[el]) {
+                copy.push(el);
+            } else if(!copy[copy.length-1]) {
+                return false
+            } else if(el === bracket[copy[copy.length-1]]) {
+                copy.pop()
+            }
+        }
+        return copy.length === 0
+    }
+
+    for(let i = 0; i < s.length; i++ ) {
+        s = s.slice(1) + s[0];
+        if(isRight(s)) {
+            answer++
+        }
+    }
+    return answer;
+}
