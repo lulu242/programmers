4ef178faa7c47a71013fb891cac4e3c771b38450
lulu242 <kgsol3242@gmail.com>
2023-10-26 16:42:58 +0900

    [level 1] Title: 2016년, Time: 3.47 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,22 +1,5 @@
 function solution(a, b) {
-    let day = ['THU','FRI','SAT','SUN','MON','TUE','WED'];
-    let days = {
-        1: 31,
-        2: 29,
-        3: 31,
-        4: 30,
-        5: 31,
-        6: 30,
-        7: 31,
-        8: 31,
-        9: 30,
-        10: 31,
-        11: 30,
-        12: 31
-    }
-    let total = 0;
-    for(let i = 1; i < a; i++) {
-        total += days[i]
-    }
-    return day[(total + b) % 7]
+    let day = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    let date = new Date(2016, a-1, b)
+    return day[date.getDay()]
 }
