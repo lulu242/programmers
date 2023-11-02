7097cb14ba0f81f6c7dbbcd58ce0838e42114f18
lulu242 <kgsol3242@gmail.com>
2023-11-02 15:36:15 +0900

    [level 2] Title: 튜플, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -2,12 +2,13 @@ function solution(s) {
     let answer = [];
     let arr = s.slice(2, -1).replaceAll('}','').split(',{').map(el=> el.split(','))
                 .sort((a,b) => a.length-b.length)
-    for(let i = 0 ; i < arr.length; i++) {
-        for(let j = 0 ; j < arr[i].length; j++) {
-            answer.includes(Number(arr[i][j])) ? null : answer.push(Number(arr[i][j]))
-        }
-    }          
-    return answer;
+                .reduce((acc, cur) => [...acc, ...cur], [])
+    // for(let i = 0 ; i < arr.length; i++) {
+        // for(let j = 0 ; j < arr[i].length; j++) {
+        //     answer.includes(Number(arr[i][j])) ? null : answer.push(Number(arr[i][j]))
+        // }   
+    // }     
+    return [...new Set(arr)].map(el=>Number(el));
 }
 
 
