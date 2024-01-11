9160ee9f495b367dcbadf93c5917d754f29c919d
lulu242 <kgsol3242@gmail.com>
2024-01-12 07:26:42 +0900

    [level Lv.1] Title: 덧칠하기, Time: 1.49 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/Lv.1/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,10 @@
 function solution(n, m, section) {
     let answer = 0;
-    let nextPart = 0;
-    section.forEach((part, idx) => {
-        if(idx === nextPart) {
+    let paintEnd = 0;
+    section.forEach(part => {
+        if(part > paintEnd) {
             answer++
-            const paintEnd = part + m - 1
-            nextPart =  section.findIndex(el => el > paintEnd)            
+            paintEnd = part + m - 1      
         }
     })
     return answer;
