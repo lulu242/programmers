33a0de85d96f4629bfada1da2e5a811722d9953e
lulu242 <kgsol3242@gmail.com>
2024-01-13 07:07:56 +0900

    [level Lv.2] Title: 전화번호 목록, Time: 122.89 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/Lv.2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,14 @@
 function solution(phone_book) {
     var answer = true;
     phone_book.sort()
+    console.log(phone_book)
     for(let i = 0; i < phone_book.length - 1; i++) {
+        
         if(phone_book[i+1].startsWith(phone_book[i])) {
                 return answer = false
         }
     }
+    
     return answer;
 }
 
